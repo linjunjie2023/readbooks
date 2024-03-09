524a6511037200dfa06ff7f8089fb194434f261c
linjunjie <3185058251@qq.com>
2024-03-09 15:46:53 +0800

    #home首页的搜索框部分
    （1）把每个部分都抽出来，因为写一起太密集了

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:readbook/theme/colors.dart';
 
 class HomePage extends StatefulWidget {
@@ -24,23 +25,65 @@ class _HomePageState extends State<HomePage> {
     return SafeArea(
       child: SingleChildScrollView(
           padding: EdgeInsets.all(15),
-          child:
-              Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-            SizedBox(
-              height: 10,
-            ),
-            Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
-              Text(
-                '你回来啦',
-                style: TextStyle(fontSize: 22, fontWeight: FontWeight.w600),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(
+                height: 10,
               ),
-              CircleAvatar(
-                backgroundImage: NetworkImage(
-                  "https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTd8fHByb2ZpbGV8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60",
-                ),
-              )
-            ])
-          ])),
+              getHomeHeder(),
+              SizedBox(
+                height: 10,
+              ),
+              getHomeSearch(),
+              SizedBox(
+                height: 15,
+              ),
+            ],
+          )),
+    );
+  }
+
+  Widget getHomeHeder() {
+    return Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: const [
+          Text(
+            '你回来啦',
+            style: TextStyle(fontSize: 22, fontWeight: FontWeight.w600),
+          ),
+          CircleAvatar(
+            backgroundImage: NetworkImage(
+              "https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTd8fHByb2ZpbGV8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60",
+            ),
+          )
+        ]);
+  }
+
+  Widget getHomeSearch() {
+    var size = MediaQuery.of(context).size;
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Container(
+          width: size.width * 0.82,
+          height: 40,
+          decoration: BoxDecoration(
+              color: grey.withOpacity(0.25),
+              borderRadius: BorderRadius.circular(12)),
+          child: Padding(
+            padding: const EdgeInsets.only(left: 15, right: 15, bottom: 0),
+            child: TextField(
+              cursorColor: primary,
+              decoration: InputDecoration(
+                border: InputBorder.none,
+                hintText: "Search...",
+              ),
+            ),
+          ),
+        ),
+        IconButton(onPressed: () {}, icon: Icon(Icons.search))
+      ],
     );
   }
 }
